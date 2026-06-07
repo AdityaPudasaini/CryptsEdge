c118d2f0292497d1be8a7b70d60b7132ff72f837
AdityaPudasaini <workingaditya00@gmail.com>
2026-06-07 14:53:07 +0545

    Setup frame

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main() {
-    
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "CryptsEdge", sf::Style::Titlebar | sf::Style::Close);
+    window.setFramerateLimit(60);
+
+    while(window.isOpen()) {
+        
+        while(const auto event = window.pollEvent()) {
+            
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+    }
+
+    window.clear();
+    window.display();
 
     return 0;
 }
--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -1,10 +1,12 @@
 #include "Player.hpp"
 
 Player::Player() {
-    position = sf::Vector2f(100.f, 600.f);
+    position = sf::Vector2f(100.f, 900.f);
     health = 100.f;
     mana = 50.f;
     isOnGround = true;
     isFacingRight = true;
     currentState = AnimationState::Idle;
 }
+
+
