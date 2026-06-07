3611e327513ac2ddfd92a71b099f4af46108cf8f
AdityaPudasaini <workingaditya00@gmail.com>
2026-06-07 14:36:44 +0545

    Initialized variables

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -1,2 +1,10 @@
 #include "Player.hpp"
 
+Player::Player() {
+    position = sf::Vector2f(100.f, 600.f);
+    health = 100.f;
+    mana = 50.f;
+    isOnGround = true;
+    isFacingRight = true;
+    currentState = AnimationState::Idle;
+}
