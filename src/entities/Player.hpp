ea7ab949108683f037841d35e758de5249c49d1d
AdityaPudasaini <workingaditya00@gmail.com>
2026-06-07 12:03:43 +0545

    Added needed functions

--- src/entities/Player.hpp
+++ src/entities/Player.hpp
@@ -1,24 +1,50 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <string>
+ #include <optional>
 
-struct Player
+class Player
 {
-    sf::Vector2f position;
-    int velocity;
-    float health, mana;
-    bool isOnGround, isFacingRight;
+    private:
+        sf::Vector2f position, velocity;
+        float health, mana;
+        bool isOnGround, isFacingRight;
 
-    enum class currentAnimationState {
-        Idle,
-        Running,
-        Attacking,
-        Hurt,
-        Dead
-    };
+        enum class AnimationState {
+            Idle,
+            Running,
+            Attacking,
+            Hurt,
+            Dead
+        };
 
-    sf::Texture texture;
-    sf::Sprite sprite;
+        AnimationState currentState;
 
-    
+        sf::Texture texture;
+        std::optional<sf::Sprite> sprite;
+
+    public:
+        Player();
+
+        ~Player();
+
+        sf::Vector2f getPosition() const;
+        float getHealth() const;
+        float getMana() const;
+
+        void handleInput();
+
+        void updatePosition(float timePassed);
+
+        void jump();
+
+        void meleeAttack();
+
+        void rangedAttack();
+
+        void damageTaken(float damageAmount);
+
+        void draw(sf::RenderWindow& window);
+
+        sf::FloatRect getHitbox();
 };
